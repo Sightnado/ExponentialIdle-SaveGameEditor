7ff543442b20111b5673928b766ed278cf0dc202
Sightnado <47155841+Sightnado@users.noreply.github.com>
2023-11-18 04:10:41 -0500

    Update Editor.vue

--- src/pages/Editor.vue
+++ src/pages/Editor.vue
@@ -384,44 +384,6 @@
                     </template>
                 </expandable-card>
             </div>
-
-            <div class="col-12">
-                <expandable-card>
-                    <template #title>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
-                             class="bi bi-exclamation-octagon-fill me-2 text-danger"
-                             viewBox="0 0 16 16">
-                            <path
-                                d="M11.46.146A.5.5 0 0 0 11.107 0H4.893a.5.5 0 0 0-.353.146L.146 4.54A.5.5 0 0 0 0 4.893v6.214a.5.5 0 0 0 .146.353l4.394 4.394a.5.5 0 0 0 .353.146h6.214a.5.5 0 0 0 .353-.146l4.394-4.394a.5.5 0 0 0 .146-.353V4.893a.5.5 0 0 0-.146-.353L11.46.146zM8 4c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 4.995A.905.905 0 0 1 8 4zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
-                        </svg>
-                        Raw Data
-                    </template>
-                    <template #content>
-                        <div class="col-12" v-for="(data, section) of saveData.data">
-                            <div v-if="typeof data === 'object' && Object.keys(data).length > 0">
-                                <expandable-card>
-                                    <template #title>{{ Case.pascalCase2Words(section) }}</template>
-                                    <template #content>
-                                        <dynamic-input v-for="key of Object.keys(data)" v-model="saveData.data[section][key]">
-                                            <template #label>{{ Case.pascalCase2Words(key) }}</template>
-                                        </dynamic-input>
-                                    </template>
-                                </expandable-card>
-                                <hr />
-                            </div>
-                            <div v-else>
-                                <expandable-card>
-                                    <template #title>{{ Case.pascalCase2Words(section) }}</template>
-                                    <template #content>
-                                        <dynamic-input v-model="saveData.data[section]" />
-                                    </template>
-                                </expandable-card>
-                                <hr />
-                            </div>
-                        </div>
-                    </template>
-                </expandable-card>
-            </div>
         </div>
     </form>
 </template>
